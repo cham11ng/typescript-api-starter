6288f2c706a29038037f24ec8f1bdf5c651bae36
Sagar Chamling <sgr.raee@gmail.com>
2018-02-04 02:12:05 +0545

    Add supertest package and write test for home and user api

---
+++ test/api/home.spec.ts
@@ -0,0 +1,23 @@
+import { expect } from 'chai';
+import * as request from 'supertest';
+import * as HttpStatus from 'http-status-codes';
+
+import app from '../../src/index';
+
+describe('API Information', () => {
+  it('should return application information', done => {
+    request(app)
+      .get('/api')
+      .end((err, res) => {
+        const expectedResponse = {
+          name: app.locals.name,
+          version: app.locals.version
+        };
+
+        expect(res.statusCode).to.equal(HttpStatus.OK);
+        expect(res.body).to.deep.equal(expectedResponse);
+
+        done();
+      });
+  });
+});
---
+++ test/api/user.spec.ts
@@ -0,0 +1,19 @@
+import { expect } from 'chai';
+import * as request from 'supertest';
+import * as HttpStatus from 'http-status-codes';
+
+import app from '../../src/index';
+
+describe('Users API test', () => {
+  it('should return users list', done => {
+    request(app)
+      .get('/api/users')
+      .end((err, res) => {
+        expect(res.statusCode).to.equal(HttpStatus.OK);
+        expect(res.body).to.have.property('data');
+        expect(res.body.data).to.be.an('array');
+
+        done();
+      });
+  });
+});
