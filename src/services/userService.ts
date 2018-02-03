075f817603cd2c3726a77247d700850a86e509eb
Sagar Chamling <sgr.raee@gmail.com>
2018-02-04 00:55:13 +0545

    Add doc block

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -5,6 +5,11 @@ import User from '../models/User';
 import UserDetail from '../resources/domain/UserDetail';
 import * as usersTransformer from '../transformers/userTransformer';
 
+/**
+ * Fetch all users from users table.
+ *
+ * @returns {Bluebird<UserDetail[]>}
+ */
 export function fetchAll(): Bluebird<UserDetail[]> {
   return User.fetchAll().then((users: Collection<Model<User>>) => usersTransformer.transformAll(users.serialize()));
 }
--- src/transformers/userTransformer.ts
+++ src/transformers/userTransformer.ts
@@ -1,9 +1,21 @@
 import UserDetail from '../resources/domain/UserDetail';
 
+/**
+ * Transform all the users detail.
+ *
+ * @param  {UserDetail[]} users
+ * @returns <UserDetail[]>
+ */
 export function transformAll(users: UserDetail[]): UserDetail[] {
   return users.map((user: UserDetail) => transform(user));
 }
 
+/**
+ * Transform user detail.
+ *
+ * @param  {UserDetail} user
+ * @returns {UserDetail}
+ */
 export function transform(user: UserDetail): UserDetail {
   return {
     name: user.name,
