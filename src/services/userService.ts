fdfb56df73af0b224fdf00583606a520d7136227
Sagar Chamling <sgr.raee@gmail.com>
2018-02-08 19:19:15 +0545

    Create transformer util

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -2,8 +2,8 @@ import Bluebird from 'bluebird';
 import { Collection, Model } from 'bookshelf';
 
 import User from '../models/User';
+import transform from '../utils/transform';
 import UserDetail from '../resources/domain/UserDetail';
-import * as usersTransformer from '../transformers/userTransformer';
 
 /**
  * Fetch all users from users table.
@@ -11,5 +11,20 @@ import * as usersTransformer from '../transformers/userTransformer';
  * @returns {Bluebird<UserDetail[]>}
  */
 export function fetchAll(): Bluebird<UserDetail[]> {
-  return User.fetchAll().then((users: Collection<Model<User>>) => usersTransformer.transformAll(users.serialize()));
+  return User.fetchAll().then((users: Collection<Model<User>>) => transform(users.serialize(), userTransform));
+}
+
+/**
+ * Transform user detail.
+ *
+ * @param  {UserDetail} user
+ * @returns {UserDetail}
+ */
+export function userTransform(user: UserDetail): UserDetail {
+  return {
+    name: user.name,
+    email: user.email,
+    updatedAt: new Date(user.updatedAt).toLocaleString(),
+    createdAt: new Date(user.updatedAt).toLocaleString()
+  };
 }
---
+++ src/utils/transform.ts
@@ -0,0 +1,10 @@
+/**
+ * Transform all the given detail with given transformation callback.
+ *
+ * @param  {T[]} data
+ * @param  {(info:T)=>T} transformCallback
+ * @returns T
+ */
+export default function transform<T>(data: T[], transformCallback: (info: T) => T): T[] {
+  return data.map(transformCallback);
+}
