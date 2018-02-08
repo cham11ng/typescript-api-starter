42b8a44855b2c4a94bc9cf41d5f36cb2a27388f6
Sagar Chamling <sgr.raee@gmail.com>
2018-02-08 20:30:03 +0545

    Minor changes

--- src/database/factories/userFactory.ts
+++ src/database/factories/userFactory.ts
@@ -2,6 +2,11 @@ import * as faker from 'faker';
 
 import UserDetail from '../../domain/entities/UserDetail';
 
+/**
+ * Retunrs user fake data.
+ *
+ * @returns UserDetail
+ */
 export default function userFactory(): UserDetail {
   const now = new Date().toString();
 
