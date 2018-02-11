4bd34b638587b33475a9caa78ce355e58f73ce2c
Sagar Chamling <sgr.raee@gmail.com>
2018-02-11 18:59:52 +0545

    Update type in faker util

--- src/utils/faker.ts
+++ src/utils/faker.ts
@@ -1,11 +1,11 @@
 /**
  * Generate fake data from given factory callback.
  *
- * @param {(()=>T)} factoryCallback
+ * @param {(()=>Promise<T>)} factoryCallback
  * @param {number=1} total
  * @returns {Promise<T[]>}
  */
-export async function generate<T>(factoryCallback: (() => T), total: number = 1): Promise<T[]> {
+export async function generate<T>(factoryCallback: (() => Promise<T>), total: number = 1): Promise<T[]> {
   const data = [];
   for (let i = 0; i < total; i++) {
     data[i] = await factoryCallback();
