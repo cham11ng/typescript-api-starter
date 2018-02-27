c6b10179dbb09bc4fbe6d4ad2a18303184b1da35
Sagar Chamling <sagarchamling@lftechnology.com>
2018-02-27 21:23:05 +0545

    Import package.json instead using fs and add json typing

--- src/config/app.ts
+++ src/config/app.ts
@@ -1,14 +1,11 @@
-import * as fs from 'fs';
 import * as dotenv from 'dotenv';
+import * as pkg from '../../package.json';
 
 dotenv.config();
 
-const contents: string = fs.readFileSync('./package.json', { encoding: 'utf8' });
-const { name, version } = JSON.parse(contents);
-
 export default {
-  name,
-  version,
+  name: (pkg as any).name,
+  version: (pkg as any).version,
   host: process.env.APP_HOST || '127.0.0.1',
   port: (process.env.NODE_ENV === 'test' ? process.env.TEST_APP_PORT : process.env.APP_PORT) || '8000',
   pagination: {
