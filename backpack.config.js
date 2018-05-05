ec09c53f4b32b4476bc6c74596f69bb1be9edde0
Sagar Chamling <sagarchamling@lftechnology.com>
2018-05-05 15:24:53 +0545

    Configure backpack and remove nodemon configuration

---
+++ backpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+
+const PRODUCTION = 'production';
+
+module.exports = {
+  webpack: (config, options) => {
+    const isProduction = options.env === PRODUCTION;
+
+    config.context = path.resolve(__dirname, 'src');
+    config.entry.main = './index.ts';
+    config.output.path = path.resolve(__dirname, 'build');
+    config.resolve.extensions = ['.ts', '.json'];
+
+    config.module.rules = [
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        loader: 'ts-loader',
+        options: {
+          transpileOnly: true
+        }
+      },
+      {
+        test: /\.json$/,
+        exclude: /node_modules/,
+        loader: 'json-loader'
+      }
+    ];
+
+    config.plugins.push(
+      new ForkTsCheckerWebpackPlugin({
+        tsconfig: path.resolve(__dirname, 'tsconfig.json')
+      })
+    );
+
+    if (isProduction) {
+      config.plugins.push(
+        new UglifyJSPlugin({
+          sourceMap: true
+        })
+      );
+    }
+
+    return config;
+  }
+};
