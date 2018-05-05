8db647627b83deed9045c55c1a812c88600c4618
Sagar Chamling <sagarchamling@lftechnology.com>
2018-05-05 16:43:57 +0545

    Add validate util

---
+++ src/utils/validate.ts
@@ -0,0 +1,12 @@
+import * as Joi from 'joi';
+
+/**
+ * Utility helper for Joi validation.
+ *
+ * @param <T> data
+ * @param <Joi.SchemaLike> schema
+ * @returns <Promise>
+ */
+export default async function validate<T>(data: T, schema: Joi.SchemaLike) {
+  await Joi.validate(data, schema, { abortEarly: false });
+}
