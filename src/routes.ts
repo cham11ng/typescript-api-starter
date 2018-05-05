652b5c2bbf64acc87f027b9351d6b474eb711c45
Sagar Chamling <sagarchamling@lftechnology.com>
2018-05-05 16:46:10 +0545

    Add validateUser middleware in user POST request

--- src/routes.ts
+++ src/routes.ts
@@ -2,12 +2,13 @@ import { Router } from 'express';
 
 import * as homeController from './controllers/home';
 import * as userController from './controllers/user';
+import { validateUserRequest } from './validators/userValidator';
 
 const router: Router = Router();
 
 router.get('/', homeController.index);
 
 router.get('/users', userController.index);
-router.post('/users', userController.store);
+router.post('/users', validateUserRequest, userController.store);
 
 export default router;
