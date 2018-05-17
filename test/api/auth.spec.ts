e24319a98c00a88b8ae50a133e54021f8bc61fa9
Sagar Chamling <sagarchamling@lftechnology.com>
2018-05-17 22:35:57 +0545

    Write integration test for /login

--- test/api/auth.spec.ts
+++ test/api/auth.spec.ts
@@ -3,74 +3,71 @@ import request from 'supertest';
 import * as HttpStatus from 'http-status-codes';
 
 import app from '../../src/app';
+import Role from '../../src/resources/enums/Role';
 import { getRandomElement } from '../../src/utils/array';
+import * as userService from '../../src/services/userService';
 
-describe('Users API test', () => {
-  test('should return users list.', () => {
-    const expectedResponse = {
-      code: HttpStatus.OK,
-      message: expect.any(String),
-      data: expect.any(Array)
-    };
-    const userResponse = {
-      name: expect.any(String),
-      email: expect.any(String),
-      updatedAt: expect.any(String),
-      createdAt: expect.any(String)
-    };
+describe('Login API test', () => {
+  const user = {
+    roleId: Role.NORMAL_USER,
+    name: faker.name.findName(),
+    email: faker.internet.email(),
+    password: faker.internet.password()
+  };
+  const { email, password } = user;
 
-    return request(app)
-      .get('/users')
-      .then(res => {
-        const userInfo = getRandomElement(res.body.data);
-
-        expect(res.status).toBe(HttpStatus.OK);
-        expect(res.body).toEqual(expectedResponse);
-        expect(userInfo).toEqual(userResponse);
-      });
+  beforeAll(async () => {
+    await userService.insert(user);
   });
 
-  test('should successfully insert user detail into database and return inserted user detail.', () => {
-    const userBody = {
-      name: faker.name.findName(),
-      email: faker.internet.email(),
-      password: faker.internet.password()
-    };
+  test('should successfully logged in with valid credentials.', () => {
     const expectedResponse = {
       code: HttpStatus.OK,
       message: expect.any(String),
       data: {
-        ...userBody,
-        id: expect.any(Number),
-        roleId: expect.any(Number),
-        password: expect.any(String),
-        createdAt: expect.any(String),
-        updatedAt: expect.any(String)
+        accessToken: expect.any(String),
+        refreshToken: expect.any(String)
       }
     };
 
     return request(app)
-      .post('/users')
-      .send(userBody)
+      .post('/login')
+      .send({ email, password })
       .then(res => {
         expect(res.status).toBe(HttpStatus.OK);
         expect(res.body).toEqual(expectedResponse);
       });
   });
 
-  test('should fail request when payload is incorrect.', () => {
+  test('should fail login with invalid credentials.', () => {
+    const expectedResponse = {
+      code: HttpStatus.UNAUTHORIZED,
+      message: expect.any(String)
+    };
+
+    return request(app)
+      .post('/login')
+      .send({ email, password: `${password}${password}` })
+      .then(res => {
+        expect(res.status).toBe(HttpStatus.UNAUTHORIZED);
+        expect(res.body).toEqual(expectedResponse);
+      });
+  });
+
+  test('should fail login without login payload.', () => {
     const expectedResponse = {
       code: HttpStatus.BAD_REQUEST,
       message: expect.any(String),
       data: expect.any(Array)
     };
+
     const badRequestResponse = {
       param: expect.any(String),
       message: expect.any(String)
     };
 
     return request(app)
-      .post('/users')
+      .post('/login')
       .then(res => {
         const errorResponse = getRandomElement(res.body.data);
 
