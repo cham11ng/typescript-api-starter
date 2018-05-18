e9b8c3dc52b078b959300992855f8cdbfe7b1b58
Sagar Chamling <sagarchamling@lftechnology.com>
2018-05-18 10:43:12 +0545

    Write services for refresh token and logout

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -3,12 +3,14 @@ import * as jwt from '../utils/jwt';
 import logger from '../utils/logger';
 import config from '../config/config';
 import * as bcrypt from '../utils/bcrypt';
+import UserSession from '../models/UserSession';
+import JWTPayload from '../domain/misc/JWTPayload';
+import ForbiddenError from '../exceptions/ForbiddenError';
 import LoginPayload from '../domain/requests/LoginPayload';
-
 import * as sessionService from '../services/sessionService';
 import UnauthorizedError from '../exceptions/UnauthorizedError';
 
-const { messages } = config;
+const { errors, messages } = config;
 
 /**
  * Create user session for valid user login.
@@ -18,54 +20,65 @@ const { messages } = config;
  * @returns {object}
  */
 export async function login(loginPayload: LoginPayload) {
-  try {
-    const { email, password } = loginPayload;
+  const { email, password } = loginPayload;
 
-    const user = await new User({ email }).fetch();
-    logger.debug('Login: Fetched user by email - ', user);
+  const user = await new User({ email }).fetch();
+  logger.debug('Login: Fetched user by email - ', user);
 
-    if (user) {
-      logger.debug(`Login: Comparing request password - ${password} and hashed password - ${user.attributes.password}`);
-      const isSame = await bcrypt.compare(password, user.attributes.password);
-      logger.debug('Login: Password match status -', isSame);
+  if (user) {
+    logger.debug(`Login: Comparing request password - ${password} and hashed password - ${user.attributes.password}`);
+    const isSame = await bcrypt.compare(password, user.attributes.password);
+    logger.debug('Login: Password match status -', isSame);
 
-      if (isSame) {
-        const { name, roleId, id: userId } = user.attributes;
-        const loggedInUser = { name, email, userId, roleId };
-        const refreshToken = jwt.generateRefreshToken(loggedInUser);
-        const userSessionPayload = { userId, token: refreshToken };
-        const session = await sessionService.create(userSessionPayload);
-        const accessToken = jwt.generateAccessToken({ ...loggedInUser, sessionId: session.id });
+    if (isSame) {
+      const { name, roleId, id: userId } = user.attributes;
+      const loggedInUser = { name, email, userId, roleId };
+      const refreshToken = jwt.generateRefreshToken(loggedInUser);
+      const userSessionPayload = { userId, token: refreshToken };
+      const session = await sessionService.create(userSessionPayload);
+      const accessToken = jwt.generateAccessToken({ ...loggedInUser, sessionId: session.id });
 
-        return { refreshToken, accessToken };
-      }
+      return { refreshToken, accessToken };
     }
-
-    throw new UnauthorizedError(messages.auth.invalidCredentials);
-  } catch (err) {
-    throw err;
   }
+
+  throw new UnauthorizedError(errors.invalidCredentials);
 }
 
 /**
- * Generate new access token for valid refresh token
+ * Refresh new access token.
  *
  * @export
  * @param {string} token
+ * @param {jwtPayload} jwtPayload
  * @returns {string}
  */
-export async function refresh(token: string) {
-  return;
+export async function refresh(token: string, jwtPayload: JWTPayload) {
+  logger.debug('User Session: Fetching session of token -', token);
+  const session = await new UserSession({ token, isActive: true }).fetch();
+  logger.debug('User Session: fetched session -', JSON.stringify(session, null, 2));
+
+  if (!session) {
+    throw new ForbiddenError(errors.sessionNotMaintained);
+  }
+
+  const accessToken = jwt.generateAccessToken({ ...jwtPayload, sessionId: session.id });
+  logger.debug('JWT: New access token generated -', accessToken);
+
+  return {
+    accessToken
+  };
 }
 
 /**
- * Remove refresh token of the user if it exists and respond accordingly.
+ * Remove user session.
  *
- * @export
  * @param {string} token
- * @returns {object}
- * @throws {error}
  */
 export async function logout(token: string) {
-  return;
+  const session = await sessionService.remove(token);
+
+  if (!session) {
+    throw new ForbiddenError(messages.auth.invalidToken);
+  }
 }
--- src/services/sessionService.ts
+++ src/services/sessionService.ts
@@ -1,8 +1,12 @@
 import logger from '../utils/logger';
+import config from '../config/config';
 import * as object from '../utils/object';
 import UserSession from '../models/UserSession';
+import ForbiddenError from '../exceptions/ForbiddenError';
 import UserSessionDetail from '../domain/entities/UserSessionDetail';
 import UserSessionPayload from '../domain/requests/UserSessionPayload';
+
+const { messages } = config;
 
 /**
  * Insert user from given user payload.
@@ -12,8 +16,27 @@ import UserSessionPayload from '../domain/requests/UserSessionPayload';
  */
 export async function create(params: UserSessionPayload): Promise<UserSessionDetail> {
   logger.debug('User Session: Creating session - ', JSON.stringify(params, null, 2));
-  const user = await new UserSession(params).save();
-  logger.debug('User Session: Session created successfully - ', JSON.stringify(user, null, 2));
+  const session = await new UserSession(params).save();
+  logger.debug('User Session: Session created successfully - ', JSON.stringify(session, null, 2));
 
-  return object.camelize(user.serialize());
+  return object.camelize(session.serialize());
+}
+
+/**
+ * Deactivate user session.
+ *
+ * @param {string} token
+ * @returns {Promise<UserSessionDetail>}
+ */
+export async function remove(token: string): Promise<UserSessionDetail> {
+  logger.debug('User Session: Deactivating token - ', token);
+  const session = await new UserSession().where({ token }).save({ isActive: false }, { patch: true, require: false });
+
+  if (session) {
+    logger.debug('User Session: Deactivated user session', JSON.stringify(session, null, 2));
+
+    return object.camelize(session.serialize());
+  }
+
+  throw new ForbiddenError(messages.auth.logoutFailed);
 }
