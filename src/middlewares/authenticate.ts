752f2061a50897867be0b954fa01db54357aaf20
Sagar Chamling <sagarchamling@lftechnology.com>
2018-05-18 10:42:27 +0545

    Add token verification middleware

---
+++ src/middlewares/authenticate.ts
@@ -0,0 +1,52 @@
+import { Request, Response, NextFunction } from 'express';
+
+import * as jwt from '../utils/jwt';
+import logger from '../utils/logger';
+import config from '../config/config';
+import JWTError from './../resources/enums/JWTError';
+import BadRequestError from '../exceptions/BadRequestError';
+import UnauthorizedError from '../exceptions/UnauthorizedError';
+
+const { errors } = config;
+
+const tokenErrorMessageMap: any = {
+  [JWTError.INVALID]: errors.invalidToken,
+  [JWTError.EXPIRED]: errors.accessTokenExpired
+};
+
+/**
+ * A middleware to authenticate the authorization token i.e. access token.
+ *
+ * @param {Request} req
+ * @param {Response} res
+ * @param {NextFunction} next
+ */
+async function authenticate(req: Request, res: Response, next: NextFunction) {
+  try {
+    res.locals.accessToken = String(req.headers.authorization).replace('Bearer ', '');
+
+    if (!req.headers.authorization || !res.locals.accessToken) {
+      throw new BadRequestError(errors.noToken);
+    }
+
+    logger.debug('JWT: Verifying token - ', req.headers.authorization, res.locals.accessToken);
+    const response: any = jwt.verifyAccessToken(res.locals.accessToken);
+    res.locals.loggedInPayload = response.encryptedData;
+    logger.debug('JWT: Authentication verified - ', JSON.stringify(res.locals.loggedInPayload, null, 2));
+
+    next();
+  } catch (err) {
+    const tokenErrorMessage = tokenErrorMessageMap[err.name];
+    logger.error('JWT: Authentication failed -', err.message);
+
+    if (tokenErrorMessage) {
+      logger.error('JWT: Token error -', tokenErrorMessage);
+
+      next(new UnauthorizedError(tokenErrorMessage));
+    } else {
+      next(err);
+    }
+  }
+}
+
+export default authenticate;
---
+++ src/middlewares/validateRefreshToken.ts
@@ -0,0 +1,52 @@
+import { Request, Response, NextFunction } from 'express';
+
+import * as jwt from '../utils/jwt';
+import logger from '../utils/logger';
+import config from '../config/config';
+import JWTError from './../resources/enums/JWTError';
+import BadRequestError from '../exceptions/BadRequestError';
+import UnauthorizedError from '../exceptions/UnauthorizedError';
+
+const { errors } = config;
+
+const tokenErrorMessageMap: any = {
+  [JWTError.INVALID]: errors.invalidToken,
+  [JWTError.EXPIRED]: errors.refreshTokenExpired
+};
+
+/**
+ * A middleware to validateRefershToken the authorization token i.e. refresh token.
+ *
+ * @param {Request} req
+ * @param {Response} res
+ * @param {NextFunction} next
+ */
+async function validateRefreshToken(req: Request, res: Response, next: NextFunction) {
+  try {
+    res.locals.refreshToken = String(req.headers.authorization).replace('Bearer ', '');
+
+    if (!req.headers.authorization || !res.locals.refreshToken) {
+      throw new BadRequestError(errors.noToken);
+    }
+
+    logger.debug('JWT: Verifying token - ', res.locals.refreshToken);
+    const response: any = jwt.verifyRefreshToken(res.locals.refreshToken);
+    res.locals.jwtPayload = response.encryptedData;
+    logger.debug('JWT: Authentication verified - ', JSON.stringify(res.locals.jwtPayload, null, 2));
+
+    next();
+  } catch (err) {
+    const tokenErrorMessage = tokenErrorMessageMap[err.name];
+    logger.error('JWT: Authentication failed -', err.message);
+
+    if (tokenErrorMessage) {
+      logger.error('JWT: Token error -', tokenErrorMessage);
+
+      next(new UnauthorizedError(tokenErrorMessage));
+    } else {
+      next(err);
+    }
+  }
+}
+
+export default validateRefreshToken;
