14332ecc56f9f65c3a0d6dda6e69f67e305d31a6
Sagar Chamling <sagarchamling@lftechnology.com>
2018-05-18 10:44:20 +0545

    Add /refresh and /logout endpoint and add respective middleware

--- src/routes.ts
+++ src/routes.ts
@@ -3,18 +3,20 @@ import { Router } from 'express';
 import * as homeController from './controllers/home';
 import * as userController from './controllers/user';
 import * as authController from './controllers/auth';
+import authenticate from './middlewares/authenticate';
 import { validateUserRequest } from './validators/userValidator';
 import { validateLoginRequest } from './validators/loginValidator';
+import validateRefreshToken from './middlewares/validateRefreshToken';
 
 const router: Router = Router();
 
 router.get('/', homeController.index);
 
 router.post('/login', validateLoginRequest, authController.login);
-router.post('/refresh', authController.refresh);
-router.post('/logout', authController.logout);
+router.post('/refresh', validateRefreshToken, authController.refresh);
+router.post('/logout', validateRefreshToken, authController.logout);
 
 router.get('/users', userController.index);
-router.post('/users', validateUserRequest, userController.store);
+router.post('/users', authenticate, validateUserRequest, userController.store);
 
 export default router;
