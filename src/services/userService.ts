54fbb1f5472f58dc43779cc4969721c59a108918
Sagar Chamling <sagarchamling@lftechnology.com>
2018-05-20 02:32:05 +0545

    Fix doc blocks and indentation

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,10 +1,8 @@
 import User from '../models/User';
-
 import logger from '../utils/logger';
 import * as bcrypt from '../utils/bcrypt';
 import * as object from '../utils/object';
 import transform from '../utils/transform';
-
 import Role from '../resources/enums/Role';
 import UserDetail from '../domain/entities/UserDetail';
 import UserPayload from '../domain/requests/UserPayload';
@@ -16,9 +14,7 @@ import UserPayload from '../domain/requests/UserPayload';
  */
 export async function fetchAll(): Promise<UserDetail[]> {
   logger.debug('Fetching users from database:');
-
   const users = await User.fetchAll();
-
   logger.debug('Fetched all users successfully:', JSON.stringify(users, null, 2));
 
   return transform(users.serialize(), (user: UserDetail) => ({
@@ -37,10 +33,8 @@ export async function fetchAll(): Promise<UserDetail[]> {
  */
 export async function insert(params: UserPayload): Promise<UserDetail> {
   logger.debug('Inserting user into database:', JSON.stringify(params, null, 2));
-
   const password = await bcrypt.hash(params.password);
   const user = await new User({ ...params, password, roleId: Role.NORMAL_USER }).save();
-
   logger.debug('Inserted user successfully:', JSON.stringify(user, null, 2));
 
   return object.camelize(user.serialize());
