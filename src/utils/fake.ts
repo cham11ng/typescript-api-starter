7ae6836a1cdf9acf7956711ea26534ce4972fa1f
Sagar Chamling <sgr.raee@gmail.com>
2019-02-28 12:59:37 +0545

    Lint fix changes

--- src/utils/fake.ts
+++ src/utils/fake.ts
@@ -5,7 +5,7 @@
  * @param {number=1} total
  * @returns {Promise<T[]>}
  */
-export async function generate<T>(factoryCallback: (() => Promise<T>), total: number = 1): Promise<T[]> {
+export async function generate<T>(factoryCallback: () => Promise<T>, total: number = 1): Promise<T[]> {
   const data = [];
   for (let i = 0; i < total; i++) {
     data[i] = await factoryCallback();
