cfec24d26c71ebb66a9293340d821320784b1b4d
Sagar Chamling <sgr.raee@gmail.com>
2019-07-30 11:03:36 +0545

    Change implementation of logger

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,36 +1,42 @@
-import 'winston-daily-rotate-file';
-
-import * as fs from 'fs';
-import * as winston from 'winston';
+import fs from 'fs';
+import DailyRotateFile from 'winston-daily-rotate-file';
+import { createLogger, format, transports } from 'winston';
 
 import app from '../config/config';
 
-const { level, maxSize, maxFiles, datePattern, dir: logDir } = app.logging;
+const { environment, logging } = app;
+const { combine, colorize, align, splat, simple, printf, timestamp } = format;
+// const { level, maxSize, maxFiles, datePattern, dir: logDir } = app.logging;
 
-if (!fs.existsSync(logDir)) {
-  fs.mkdirSync(logDir);
+const formatter = printf((info: any) => {
+  const { level, message, timestamp: ts, ...meta } = info;
+
+  return `[${ts}] - [${level}] ${message} ${JSON.stringify(meta, null, 2) || ''}`;
+});
+
+if (!fs.existsSync(logging.dir)) {
+  fs.mkdirSync(logging.dir);
 }
 
-const transports = [
-  new winston.transports.DailyRotateFile({
-    maxSize,
-    maxFiles,
-    datePattern,
-    zippedArchive: true,
-    filename: `${logDir}/${level}-%DATE%.log`
-  })
-];
+let trans: any = [];
 
-if (process.env.NODE_ENV === 'development') {
-  transports.push(
-    new winston.transports.Console({
-      level,
-      colorize: true,
-      timestamp: () => new Date().toISOString()
+if (environment === 'development') {
+  trans = [new transports.Console()];
+}
+
+const logger = createLogger({
+  level: logging.level,
+  format: combine(splat(), simple(), colorize(), timestamp(), align(), formatter),
+  transports: [
+    ...trans,
+    new DailyRotateFile({
+      maxSize: logging.maxSize,
+      maxFiles: logging.maxFiles,
+      datePattern: logging.datePattern,
+      zippedArchive: true,
+      filename: `${logging.dir}/${logging.level}-%DATE%.log`
     })
-  );
-}
-
-const logger = new winston.Logger({ transports });
+  ]
+});
 
 export default logger;
