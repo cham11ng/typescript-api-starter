52501653382e870e374446746cc327571b8699c5
Sagar Chamling <sgr.raee@gmail.com>
2019-07-30 10:59:43 +0545

    Update configuration of babel/backpack/jest

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  verbose: true,
+  testEnvironment: 'node',
+  coverageDirectory: 'coverage',
+  testMatch: ['**/*/*.spec.ts'],
+  moduleFileExtensions: ['js', 'ts'],
+  moduleDirectories: ['node_modules']
+};
