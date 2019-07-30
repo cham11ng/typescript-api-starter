6eaaaeabdc173f28343a6c19c19d146a7523d4cc
Sagar Chamling <sgr.raee@gmail.com>
2019-07-30 11:06:19 +0545

    Fix migration and seeder

--- src/database/seeds/user_table_seeder.ts
+++ src/database/seeds/user_table_seeder.ts
@@ -1,23 +1,20 @@
 import * as Knex from 'knex';
-import Bluebird from 'bluebird';
 
 import * as bcrypt from '../../utils/bcrypt';
 import Role from '../../resources/enums/Role';
 import Table from '../../resources/enums/Table';
 
-export function seed(knex: Knex): Bluebird<any[]> {
-  return knex(Table.USERS)
-    .del()
-    .then(async () => {
-      return Promise.all([
-        knex(Table.USERS).insert([
-          {
-            role_id: Role.ADMIN,
-            name: 'Sagar Chamling',
-            email: 'sgr.raee@gmail.com',
-            password: await bcrypt.hash('secret')
-          }
-        ])
-      ]);
-    });
+export function seed(knex: Knex): Promise<any> {
+  return knex(Table.USERS).then(async () => {
+    return Promise.all([
+      knex(Table.USERS).insert([
+        {
+          role_id: Role.ADMIN,
+          name: 'Sagar Chamling',
+          email: 'sgr.raee@gmail.com',
+          password: await bcrypt.hash('secret')
+        }
+      ])
+    ]);
+  });
 }
