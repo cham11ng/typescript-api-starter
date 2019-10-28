aeb12790b7f92bc54a5c40920d0cccca8d5a3740
Sagar Chamling <sgr.raee@gmail.com>
2019-10-28 09:02:35 +0545

    WIP: Fix test

--- test/api/auth.spec.ts
+++ test/api/auth.spec.ts
@@ -7,7 +7,10 @@ import Role from '../../src/resources/enums/Role';
 import { clearDb, getRandomElement } from '../helper';
 import * as userService from '../../src/services/userService';
 
-describe('Login API test', () => {
+describe('Auth Workflow', () => {
+  let authorization: string;
+  let accessToken: string;
+
   const user = {
     roleId: Role.NORMAL_USER,
     name: faker.name.findName(),
@@ -20,210 +23,182 @@ describe('Login API test', () => {
     await clearDb();
 
     await userService.insert(user);
+
+    const response = await request(app)
+      .post('/login')
+      .send({ email, password });
+
+    authorization = `Bearer ${response.body.data.refreshToken}`;
   });
 
   afterAll(() => new Promise(resolve => setTimeout(() => resolve(), 500)));
 
-  test('should login successfully with valid credentials.', () => {
-    const expectedResponse = {
-      code: HttpStatus.OK,
-      message: expect.any(String),
-      data: {
-        accessToken: expect.any(String),
-        refreshToken: expect.any(String)
-      }
-    };
+  describe('Login API test', () => {
+    test('should login successfully with valid credentials.', () => {
+      const expectedResponse = {
+        code: HttpStatus.OK,
+        message: expect.any(String),
+        data: {
+          accessToken: expect.any(String),
+          refreshToken: expect.any(String)
+        }
+      };
 
-    return request(app)
-      .post('/login')
-      .send({ email, password })
-      .then(res => {
-        expect(res.status).toBe(HttpStatus.OK);
-        expect(res.body).toEqual(expectedResponse);
-      });
+      return request(app)
+        .post('/login')
+        .send({ email, password })
+        .then(res => {
+          expect(res.status).toBe(HttpStatus.OK);
+          expect(res.body).toEqual(expectedResponse);
+        });
+    });
+
+    test('should fail login with invalid credentials.', () => {
+      const expectedResponse = {
+        code: HttpStatus.UNAUTHORIZED,
+        message: expect.any(String)
+      };
+
+      return request(app)
+        .post('/login')
+        .send({ email, password: `${password}${password}` })
+        .then(res => {
+          expect(res.status).toBe(HttpStatus.UNAUTHORIZED);
+          expect(res.body).toEqual(expectedResponse);
+        });
+    });
+
+    test('should fail login without login payload.', () => {
+      const expectedResponse = {
+        code: HttpStatus.BAD_REQUEST,
+        message: expect.any(String),
+        data: expect.any(Array)
+      };
+
+      const badRequestResponse = {
+        param: expect.any(String),
+        message: expect.any(String)
+      };
+
+      return request(app)
+        .post('/login')
+        .then(res => {
+          const errorResponse = getRandomElement(res.body.data);
+
+          expect(res.status).toBe(HttpStatus.BAD_REQUEST);
+          expect(res.body).toEqual(expectedResponse);
+          expect(errorResponse).toEqual(badRequestResponse);
+        });
+    });
   });
 
-  test('should fail login with invalid credentials.', () => {
-    const expectedResponse = {
-      code: HttpStatus.UNAUTHORIZED,
-      message: expect.any(String)
-    };
+  describe('Logout API test', () => {
+    test('should logout successfully with valid authorization token.', () => {
+      const expectedResponse = {
+        code: HttpStatus.OK,
+        message: expect.any(String)
+      };
 
-    return request(app)
-      .post('/login')
-      .send({ email, password: `${password}${password}` })
-      .then(res => {
-        expect(res.status).toBe(HttpStatus.UNAUTHORIZED);
-        expect(res.body).toEqual(expectedResponse);
-      });
+      return request(app)
+        .post('/logout')
+        .set({ authorization })
+        .then(res => {
+          expect(res.status).toBe(HttpStatus.OK);
+          expect(res.body).toEqual(expectedResponse);
+        });
+    });
+
+    test('should fail logout with same authorization token.', () => {
+      const expectedResponse = {
+        code: HttpStatus.FORBIDDEN,
+        message: expect.any(String)
+      };
+
+      return request(app)
+        .post('/logout')
+        .set({ authorization })
+        .then(res => {
+          expect(res.status).toBe(HttpStatus.FORBIDDEN);
+          expect(res.body).toEqual(expectedResponse);
+        });
+    });
+
+    test('should fail logout with invalid authorization token.', () => {
+      const expectedResponse = {
+        code: HttpStatus.UNAUTHORIZED,
+        message: expect.any(String)
+      };
+
+      return request(app)
+        .post('/logout')
+        .set({ authorization: faker.random.alphaNumeric() })
+        .then(res => {
+          expect(res.status).toBe(HttpStatus.UNAUTHORIZED);
+          expect(res.body).toEqual(expectedResponse);
+        });
+    });
   });
 
-  test('should fail login without login payload.', () => {
-    const expectedResponse = {
-      code: HttpStatus.BAD_REQUEST,
-      message: expect.any(String),
-      data: expect.any(Array)
-    };
+  describe('Refresh token API test', () => {
+    test('should refresh access token successfully with valid authorization token.', () => {
+      const expectedResponse = {
+        code: HttpStatus.OK,
+        message: expect.any(String),
+        data: {
+          accessToken: expect.any(String)
+        }
+      };
 
-    const badRequestResponse = {
-      param: expect.any(String),
-      message: expect.any(String)
-    };
+      return request(app)
+        .post('/refresh')
+        .set({ authorization })
+        .then(res => {
+          accessToken = res.body.data.accessToken;
 
-    return request(app)
-      .post('/login')
-      .then(res => {
-        const errorResponse = getRandomElement(res.body.data);
+          expect(res.status).toBe(HttpStatus.OK);
+          expect(res.body).toEqual(expectedResponse);
+        });
+    });
 
-        expect(res.status).toBe(HttpStatus.BAD_REQUEST);
-        expect(res.body).toEqual(expectedResponse);
-        expect(errorResponse).toEqual(badRequestResponse);
-      });
-  });
-});
+    test('should successfully access API with new access token.', () => {
+      const expectedResponse = {
+        code: HttpStatus.OK,
+        message: expect.any(String),
+        data: expect.any(Array)
+      };
+      const userResponse = {
+        name: expect.any(String),
+        email: expect.any(String),
+        roleId: expect.any(Number),
+        updatedAt: expect.any(String),
+        createdAt: expect.any(String)
+      };
 
-describe('Logout API test', () => {
-  let authorization: string;
-  const user = {
-    roleId: Role.NORMAL_USER,
-    name: faker.name.findName(),
-    email: 'logout-test@starter.com',
-    password: faker.internet.password()
-  };
-  const { email, password } = user;
+      return request(app)
+        .get('/users')
+        .set({ authorization: `Bearer ${accessToken}` })
+        .then(res => {
+          const userInfo = getRandomElement(res.body.data);
 
-  beforeAll(async () => {
-    await userService.insert(user);
-    const response = await request(app)
-      .post('/login')
-      .send({ email, password });
-    authorization = `Bearer ${response.body.data.refreshToken}`;
-  });
+          expect(res.status).toBe(HttpStatus.OK);
+          expect(res.body).toEqual(expectedResponse);
+          expect(userInfo).toEqual(userResponse);
+        });
+    });
 
-  test('should logout successfully with valid authorization token.', () => {
-    const expectedResponse = {
-      code: HttpStatus.OK,
-      message: expect.any(String)
-    };
+    test('should fail refresh with invalid authorization token.', () => {
+      const expectedResponse = {
+        code: HttpStatus.UNAUTHORIZED,
+        message: expect.any(String)
+      };
 
-    return request(app)
-      .post('/logout')
-      .set({ authorization })
-      .then(res => {
-        expect(res.status).toBe(HttpStatus.OK);
-        expect(res.body).toEqual(expectedResponse);
-      });
-  });
-
-  test('should fail logout with same authorization token.', () => {
-    const expectedResponse = {
-      code: HttpStatus.FORBIDDEN,
-      message: expect.any(String)
-    };
-
-    return request(app)
-      .post('/logout')
-      .set({ authorization })
-      .then(res => {
-        expect(res.status).toBe(HttpStatus.FORBIDDEN);
-        expect(res.body).toEqual(expectedResponse);
-      });
-  });
-
-  test('should fail logout with invalid authorization token.', () => {
-    const expectedResponse = {
-      code: HttpStatus.UNAUTHORIZED,
-      message: expect.any(String)
-    };
-
-    return request(app)
-      .post('/logout')
-      .set({ authorization: faker.random.alphaNumeric() })
-      .then(res => {
-        expect(res.status).toBe(HttpStatus.UNAUTHORIZED);
-        expect(res.body).toEqual(expectedResponse);
-      });
-  });
-});
-
-describe('Refresh token API test', () => {
-  let accessToken: string;
-  let authorization: string;
-
-  const user = {
-    roleId: Role.NORMAL_USER,
-    name: faker.name.findName(),
-    email: 'refresh-test@starter.com',
-    password: faker.internet.password()
-  };
-  const { email, password } = user;
-
-  beforeAll(async () => {
-    await userService.insert(user);
-    const response = await request(app)
-      .post('/login')
-      .send({ email, password });
-    authorization = `Bearer ${response.body.data.refreshToken}`;
-  });
-
-  test('should refresh access token successfully with valid authorization token.', () => {
-    const expectedResponse = {
-      code: HttpStatus.OK,
-      message: expect.any(String),
-      data: {
-        accessToken: expect.any(String)
-      }
-    };
-
-    return request(app)
-      .post('/refresh')
-      .set({ authorization })
-      .then(res => {
-        accessToken = res.body.data.accessToken;
-
-        expect(res.status).toBe(HttpStatus.OK);
-        expect(res.body).toEqual(expectedResponse);
-      });
-  });
-
-  test('should successfully access API with new access token.', () => {
-    const expectedResponse = {
-      code: HttpStatus.OK,
-      message: expect.any(String),
-      data: expect.any(Array)
-    };
-    const userResponse = {
-      name: expect.any(String),
-      email: expect.any(String),
-      roleId: expect.any(Number),
-      updatedAt: expect.any(String),
-      createdAt: expect.any(String)
-    };
-
-    return request(app)
-      .get('/users')
-      .set({ authorization: `Bearer ${accessToken}` })
-      .then(res => {
-        const userInfo = getRandomElement(res.body.data);
-
-        expect(res.status).toBe(HttpStatus.OK);
-        expect(res.body).toEqual(expectedResponse);
-        expect(userInfo).toEqual(userResponse);
-      });
-  });
-
-  test('should fail refresh with invalid authorization token.', () => {
-    const expectedResponse = {
-      code: HttpStatus.UNAUTHORIZED,
-      message: expect.any(String)
-    };
-
-    return request(app)
-      .post('/refresh')
-      .set({ authorization: faker.random.alphaNumeric() })
-      .then(res => {
-        expect(res.status).toBe(HttpStatus.UNAUTHORIZED);
-        expect(res.body).toEqual(expectedResponse);
-      });
+      return request(app)
+        .post('/refresh')
+        .set({ authorization: faker.random.alphaNumeric() })
+        .then(res => {
+          expect(res.status).toBe(HttpStatus.UNAUTHORIZED);
+          expect(res.body).toEqual(expectedResponse);
+        });
+    });
   });
 });
