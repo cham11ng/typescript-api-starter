3f9d70c2790e09915c85bacf488d6741149da69d
Sagar Chamling <sgr.raee@gmail.com>
2021-07-22 14:49:55 +0545

    Upgrade version and reconfigure eslint, prettierrc

--- .prettierrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  semi: true,
-  trailingComma: "none",
-  singleQuote: true,
-  printWidth: 80,
-  parser: 'typescript',
-  tabWidth: 2,
-};
--- test/unit/object.spec.ts
+++ test/unit/object.spec.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/camelcase */
 import { camelize, isObject } from '../../src/utils/object';
 
 describe('Utils: isObject()', () => {
