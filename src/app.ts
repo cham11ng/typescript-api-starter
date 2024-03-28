5d160bbfa265a3cf3654a6786ce9e72e0267781d
Sagar Chamling <sgr.raee@gmail.com>
2024-03-28 16:07:34 -0400

    Implement rate-limiter

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,7 @@ import helmet from 'helmet';
 import { bindModel } from './config/db';
 import genericErrorHandler from './middlewares/genericErrorHandler';
 import notFoundHandler from './middlewares/notFoundHandler';
+import rateLimitMiddleware from './middlewares/rateLimitHandler';
 import transactionHandler from './middlewares/transactionHandler';
 import routes from './routes';
 
@@ -15,6 +16,7 @@ bindModel();
 app.use(cors());
 app.use(helmet());
 app.use(transactionHandler);
+app.use(rateLimitMiddleware);
 app.use(express.json({ limit: '300kb' }));
 app.use(express.urlencoded({ extended: true }));
 
